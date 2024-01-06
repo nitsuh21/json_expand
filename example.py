0503929443b1f06eb95e05c95bf1797d90ff774e
nitsuh <nitsuh@eaglelionsystems.com>
2024-01-06 11:29:47 +0300

    mongo objectid options included

--- example.py
+++ example.py
@@ -1,5 +1,22 @@
-import replicated_data from './json_expand.py'
+from json_expander import expand_json, generate_random_id, add_prefix_suffix
 
-replicated_array = replicated_data(1000)
+json_obj = {
+    "_id": {"$oid": ""},
+    "bulkCode": "",
+    "time": "21",
+    "isActive": True,
+    "locked": False,
+    "woreda": "10"
+}
 
-print(replicated_array) 
+num_objects = 3
+
+custom_fields = {
+    "id[$oid]": lambda value: generate_random_id(10),
+    "bulkCode": lambda value: add_prefix_suffix(value, "AAU","",True,7),
+}
+
+expanded_objects = expand_json(json_obj, num_objects, False, custom_fields)
+
+for obj in expanded_objects:
+    print(obj)
--- json_expander.py
+++ json_expander.py
@@ -1,22 +1,33 @@
+from modifiers import generate_random_id, add_prefix_suffix
 import json
 import random
 import string
 
-def expand_json(json_obj, num_objects, custom_fields={}):
+def generate_mongodb_object_id():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=24))
+    
+def expand_json(json_obj, num_objects, export, custom_fields={}):
     expanded_objects = []
-
+    
     for _ in range(num_objects):
         expanded_obj = {}
         for key, value in json_obj.items():
             if key in custom_fields:
-                # Apply custom field operations
-                if custom_fields[key] == "random_id":
-                    expanded_obj[key] = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-                elif custom_fields[key] == "user_code_prefix":
-                    expanded_obj[key] = f"{custom_fields['user_code_prefix']}-{value}"
+                if callable(custom_fields[key]):
+                    expanded_obj[key] = custom_fields[key](value)
+                else:
+                    expanded_obj[key] = value
             else:
-                expanded_obj[key] = value
+                if key == "_id": 
+                    expanded_obj[key] = {"$oid": generate_mongodb_object_id()}
+                else:
+                    expanded_obj[key] = value
 
         expanded_objects.append(expanded_obj)
+    
+    if export:
+        with open("expanded.json", "w") as f:
+            json.dump(expanded_objects, f, indent=4)
 
-    return expanded_objects
+    else:
+        return expanded_objects
