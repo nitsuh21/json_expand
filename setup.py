b543fdf5f4ff09246bb89bf879c9698564edcc3f
nitsuh <nitsuh@eaglelionsystems.com>
2024-01-06 12:55:19 +0300

    adding version

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='json-expander',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         
     ],
     url='https://github.com/nitsuh21/json_expand.git'
 )
+
