b66c1a76ca3a4375c004bc560cd132daea6c4a6f
nitsuh <nitsuhdemissew21@gmail.com>
2024-01-08 15:04:31 +0300

    email changes

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ num_objects = 3
 
 custom_fields = {
     "id[$oid]": lambda value: generate_random_id(10),
-    "bulkCode": lambda value: add_prefix_suffix(value, "AAU","",True,7),
+    "bulkCode": lambda value: add_prefix_suffix(value, "AU","",True,7),
 }
 
 expanded_objects = expand_json(json_obj, num_objects, False, custom_fields)
