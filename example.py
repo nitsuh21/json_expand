2189875c14ecca6225d1776e99a4ef394c3b869f
nitsuh <nitsuhdemissew21@gmail.com>
2024-01-12 16:33:37 +0300

    pycache

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from json_expander import expand_json, generate_random_id, add_prefix_suffix
 json_obj = {
     "_id": {"$oid": ""},
     "userCode": "",
-    "time": "21",
+    "time": "12",
     "role": "student"
 }
 
@@ -11,7 +11,7 @@ num_objects = 3
 
 custom_fields = {
     "id[$oid]": lambda value: generate_random_id(10),
-    "bulkCode": lambda value: add_prefix_suffix(value, "AU","",True,7),
+    "userCode": lambda value: add_prefix_suffix(value, "AU","",True,7),
 }
 
 expanded_objects = expand_json(json_obj, num_objects, False, custom_fields)
